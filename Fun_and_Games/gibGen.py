ddff4f0f43edc2c529b943e053bfc8c3279a420d
Daniel Wilson <jordangro@gmail.com>
2017-03-29 15:37:22 -0400

    Create gibGen.py
    A continuation of the language junk from that other file.

---
+++ Fun_and_Games/gibGen.py
@@ -0,0 +1,10 @@
+def init()
+
+# Global Variables
+vowels = ['a','e','i','o','u','y']
+unVowels = ['b','c','d','f','g','h','j','k','l','m','n','p','q','qu,','r','s','t','v','x','y','z']
+uVSCombos = ['bs','cs','ds','fs','gs','hs','js','ks','ls','ms','ns','ps','qs','rs','ss','ts','vs','ys']
+uVRCombos = ['br','cr','dr','fr','gr','pr','rr','tr']
+uVOtherCombos = ['ph','tt','sh','ll','sw','ch','ck','sm','sn','sk']
+
+init()
