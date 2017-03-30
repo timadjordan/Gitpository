f09234b024597ee1105aefea5caec01260231a14
Daniel Wilson <jordangro@gmail.com>
2017-03-30 16:54:53 -0400

    Update gibGen.py

--- Fun_and_Games/gibGen.py
+++ Fun_and_Games/gibGen.py
@@ -2,6 +2,9 @@ def init()
 
 # Global Variables
 vowels = ['a','e','i','o','u','y']
+# There must be a way to use weighted random to select vowels and unVowels.
+# Math, maybe.
+# weightedAlphabet = ['e']['t']['a']['o']['i'['n']['s']['h'] + ['c', 'u', 'm', 'w', 'f', 'g', 'y', 'p']+ ['b', 'v', 'k'] + ['j', 'x', 'q', 'z']
 unVowels = ['b','c','d','f','g','h','j','k','l','m','n','p','q','qu,','r','s','t','v','x','y','z']
 uVSCombos = ['bs','cs','ds','fs','gs','hs','js','ks','ls','ms','ns','ps','qs','rs','ss','ts','vs','ys']
 uVRCombos = ['br','cr','dr','fr','gr','pr','rr','tr']
